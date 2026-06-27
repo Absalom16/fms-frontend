0eab0040f078e16386b404d87c921c3d6b757cc9
root <root@DESKTOP-50HO35A.localdomain>
2026-06-28 00:42:35 +0300

    api update

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,8 @@
 import axios, { type AxiosError } from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "/api/v1",
+  // baseURL: import.meta.env.VITE_API_URL || "/api/v1",
+  baseURL: "https://fms-backend-fudc.onrender.com/api/v1",
   headers: { "Content-Type": "application/json" },
   timeout: 30000,
 });
